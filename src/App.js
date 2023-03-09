a171dbe2c38efe000b9c3be33ed4d03989602211
eng-milan <tohid_d.milan@yahoo.com>
2023-03-08 21:11:11 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import NotificationData from "./data/NotificationData"
+import Header from "./components/Header"
+import NotificationList from "./components/NotificationList"
+import Footer from "./components/Footer"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [notificationRead, setNotificationRead] = useState(() => {
+        const initialState = NotificationData.map((user, index) => {
+            if (index < 3) {
+                return { name: user.name, isRead: false }
+            }
+            return { name: user.name, isRead: true }
+        })
+        return initialState
+    })
+
+    const onMarkAllReadClick = () => {
+        const newState = notificationRead.map((user) => {
+            return { ...user, isRead: true }
+        })
+        setNotificationRead(newState)
+    }
+
+    const onNotificationClick = (user) => {
+        const newState = notificationRead.map((data) => {
+            if (user.name === data.name) {
+                return { ...data, isRead: true }
+            }
+            return data
+        })
+        setNotificationRead(newState)
+    }
+
+    return <div className="bg-gray-50 md:py-[50px] md:px-[150px] lg:px-[250px] xl:px-[360px] 2xl:px-[500px]">
+        <div className="px-5 pt-8 pb-2 bg-white md:rounded-lg">
+            <Header state={notificationRead} handleClick={onMarkAllReadClick}></Header>
+            <NotificationList
+                data={NotificationData}
+                state={notificationRead}
+                handleNotificationClick={onNotificationClick} />
+        </div>
+        <Footer className={"mt-6"} />
     </div>
-  );
 }
 
-export default App;
+export default App
